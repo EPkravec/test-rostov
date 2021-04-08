f1738cbc03ff4e14d01091adbc663317803779b2
егор <Kravec.egor@gmail.com>
2021-04-08 21:53:32 +0400

    add class sourse + migratoin

--- main.py
+++ main.py
@@ -14,24 +14,20 @@ class Workload:
         self.size = size
 
     def creat_box(self):
-        if not self.credentals_box:
-            self.credentals_box = dict.fromkeys(
-                [self.cloud], [dict.fromkeys(['aws_access_key_id'], self.aws_access_key_id),
-                               dict.fromkeys(['ws_secret_access_key'], self.aws_secret_access_key),
-                               dict.fromkeys(['username'], self.username_aws),
-                               dict.fromkeys(['password'], self.password_aws),
-                               dict.fromkeys(['domen'], self.domen),
-                               dict.fromkeys(['ip'], self.ip),
-                               dict.fromkeys(['point'], self.point),
-                               dict.fromkeys(['size'], self.size)
-                               ]
-            )
+        self.credentals_box = dict.fromkeys(
+            [self.cloud], [dict.fromkeys(['aws_access_key_id'], self.aws_access_key_id),
+                           dict.fromkeys(['ws_secret_access_key'], self.aws_secret_access_key),
+                           dict.fromkeys(['username'], self.username_aws),
+                           dict.fromkeys(['password'], self.password_aws),
+                           dict.fromkeys(['domen'], self.domen),
+                           dict.fromkeys(['ip'], self.ip),
+                           dict.fromkeys(['point'], self.point),
+                           dict.fromkeys(['size'], self.size)
+                           ]
+        )
 
-            data_box = self.credentals_box
-            return data_box
-        else:
-            data_box = self.credentals_box
-            return data_box
+        data_box = self.credentals_box
+        return data_box
 
 
 class Credentials:
@@ -44,54 +40,87 @@ class Credentials:
         self.ip = None
 
     def check_upd(self):
+        list_upid = []
         for items in self.workload:
             if items == 'aws':
                 for values in self.workload['aws']:
                     for key, value in values.items():
                         if key == 'username':
                             self.username = value
+                            list_upid.append(self.username)
                         elif key == 'password':
                             self.password = value
-                        elif key == 'ip':
-                            self.ip = value
+                            list_upid.append(self.password)
                         elif key == 'domen':
                             self.domen = value
-        return f'username = {self.username}, password = {self.password}, domen = {self.domen}, ip = {self.ip}'
+                        elif key == 'ip':
+                            self.ip = value
+                            list_upid.append(self.ip)
+
+        return list_upid
 
 
 class MountPoint:
 
     def __init__(self, workload):
         self.workload = workload
+        self.cloud = None
         self.point = None
         self.size = None
 
     def check_ps(self):
+        list_i = []
+        list_p = []
         for items in self.workload:
             if items == 'aws':
+                self.cloud = items
+                list_i.append(self.cloud)
                 for values in self.workload['aws']:
                     for key, value in values.items():
                         if key == 'point':
                             self.point = value
+                            list_p.append(self.point)
                         elif key == 'size':
                             self.size = value
 
-        return f'point = {self.point}, size = {self.size}'
+        return list_i, list_p
+
+
+class Source:
+
+    def __init__(self, credentials):
+        self.check = credentials
+
+    def check_upi(self):
+        for i in self.check:
+            if i != None:
+                return True
+            else:
+                print('Error: absent username/password/ip')
+
+
+class MigratoinTarget:
+
+    def __init__(self, mountPoint):
+        self.cloud = mountPoint
+
+    def check_i(self):
+        if self.cloud != ['aws'] and self.cloud != ['azure'] and self.cloud != ['vsphere'] and self.cloud != ['vcloud']:
+            print('Error: connecting to the wrong cloud')
+        else:
+            return True
+
+
+# class Migration:
 
 
 a = Workload(cloud='aws', username='user1', password='12346576', ip='198.123.24.1', point='c:\\sd\\sddse',
              size='512').creat_box()
-print(a)
+
 b = Credentials(workload=a).check_upd()
-print(b)
-g = MountPoint(workload=a).check_ps()
-print(g)
 
+g_1 = MountPoint(workload=a).check_ps()[0]
 
-#
-#
-# class MigrationTarget:
-#
-#     def __init__(self):
-#         self.type_cloud = ['aws', 'azure', 'vsphere', 'vcloud']
-#         # self.credentials =
+h = Source(credentials=b).check_upi()
+
+e = MigratoinTarget(mountPoint=g_1).check_i()
